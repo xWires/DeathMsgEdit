27d3ed338398964971e9a2cd65e507773ede38f2
xWires <xwires@tangledwires.xyz>
2024-04-06 05:16:03 +0100

    Add bStats

--- src/main/java/xyz/tangledwires/deathmsgedit/DeathMsgEditPlugin.java
+++ src/main/java/xyz/tangledwires/deathmsgedit/DeathMsgEditPlugin.java
@@ -1,5 +1,7 @@
 package xyz.tangledwires.deathmsgedit;
 
+import org.bstats.bukkit.Metrics;
+
 // import java.util.ArrayList;
 
 import org.bukkit.command.Command;
@@ -13,27 +15,10 @@ import xyz.tangledwires.deathmsgedit.events.ChangeDeathMessage;
 public class DeathMsgEditPlugin extends JavaPlugin {
     @Override
     public void onEnable() {
-        /* 
-        FileConfiguration config = this.getConfig();
-        ArrayList<String> blockExplosionDefaults = new ArrayList<String>();
-        blockExplosionDefaults.add("{victim} blew up");
-        config.set("messages.BLOCK_EXPLOSION", blockExplosionDefaults);
+        int pluginId = 21523;
+		@SuppressWarnings("unused")
+		Metrics metrics = new Metrics(this, pluginId);
 
-        ArrayList<String> contactDefaults = new ArrayList<String>();
-        contactDefaults.add("{victim} was pricked to death");
-        config.set("messages.CONTACT", contactDefaults);
-
-        ArrayList<String> crammingDefaults = new ArrayList<String>();
-        crammingDefaults.add("{victim} was squished too much");
-        config.set("messages.CRAMMING", crammingDefaults);
-
-        ArrayList<String> dragonBreathDefaults = new ArrayList<String>();
-        dragonBreathDefaults.add("{victim} was roasted in dragon's breath");
-        config.set("messages.DRAGON_BREATH", dragonBreathDefaults);
-
-        config.options().copyDefaults(true);
-        */
-        
         this.saveDefaultConfig();
         getServer().getPluginManager().registerEvents(new ChangeDeathMessage(), this);
         getLogger().info("Hello, SpigotMC!");
